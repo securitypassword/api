60cc06f4c30a137f4ec2733951509528fbc0c08c
martin-sainos-demian <martin.sainos.demian@gmail.com>
2022-11-17 20:56:39 -0600

    firebase-admin

--- js/fire.js
+++ js/fire.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "firebase-admin/app";
+import { getAnalytics } from "firebase-admin/analytics";
 import firebase from 'firebase-admin'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
