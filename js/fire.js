3e4a1b13e4c42ba83f8e8ce842a8aa84c37f131b
martin-sainos-demian <martin.sainos.demian@gmail.com>
2022-11-17 21:28:12 -0600

    fire.js key pls not steal uwu

--- js/fire.js
+++ js/fire.js
@@ -5,6 +5,8 @@ import firebase from 'firebase-admin'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+var serviceAccount = require("../key/securitypassword-9ad22-firebase-adminsdk-4mlo9-b8d69d2c03.json");
+
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
@@ -18,7 +20,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = initializeApp({credential: admin.credential.cert(serviceAccount)});
 //export const analytics = getAnalytics(app);
 
 //declare database
