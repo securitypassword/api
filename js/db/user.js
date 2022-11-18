4a3b1db8831ed113afc28cbfbd25d2c868369686
martin-sainos-demian <martin.sainos.demian@gmail.com>
2022-11-17 18:08:45 -0600

    export default user

--- js/db/user.js
+++ js/db/user.js
@@ -2,23 +2,25 @@ import { collection, doc, setDoc } from "firebase/firestore";
 import db from "../fire.js"
 const rol = collection(db, "rol");
 
-export const getRoles = async function(){
+const getRoles = async function(){
   const rolSnapshot = await getDocs(rol);
   const rolList = rolSnapshot.docs.map(doc => doc.data());
   return rolList;
 }
 
-export const login = async function(username, password){
+const login = async function(username, password){
     let roles = await getRoles();
     console.log(roles)
     return true;
 }
-export const runUser = function(app){
+const runUser = function(app){
     app.get("/login", async (req, res, next) => {
-        var res = await user.login();
+        var res = await login();
         res.json({
         data: res,
         msg:"welcome >:3"
         });
     });
 }
+
+export default runUser;
