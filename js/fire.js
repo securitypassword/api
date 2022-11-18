86d222d9439d01d66df6e42056c3bcb94abe6498
martin-sainos-demian <martin.sainos.demian@gmail.com>
2022-11-17 20:48:53 -0600

    node_modulesn't

--- js/fire.js
+++ js/fire.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import firebase from 'firebase'
+import firebase from 'firebase-admin'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
