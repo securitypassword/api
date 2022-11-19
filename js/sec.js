c9a29a3c0ececfa0c5d74213c6ce98487c187c31
martin-sainos-demian <martin.sainos.demian@gmail.com>
2022-11-19 09:37:11 -0600

    buffer of key

--- js/sec.js
+++ js/sec.js
@@ -69,8 +69,8 @@ const generateKeys = function (){
         // The standard secure default length for RSA keys is 2048 bits
         modulusLength: 2048,
     })
-    fs.writeFileSync(pathPrivate, Buffer.from(privateKey), 'utf8')
-    fs.writeFileSync(pathPublic, Buffer.from(publicKey), 'utf8')
+    fs.writeFileSync(pathPrivate, Buffer.of(privateKey), 'utf8')
+    fs.writeFileSync(pathPublic, Buffer.of(publicKey), 'utf8')
     console.log("keys changed")
     console.log("private")
     console.log(privateKey)
