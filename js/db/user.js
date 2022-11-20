a19a0351de0b76bd88abdab28653a4c57f8acb1d
martin-sainos-demian <martin.sainos.demian@gmail.com>
2022-11-20 12:09:12 -0600

    register req

--- js/db/user.js
+++ js/db/user.js
@@ -131,7 +131,7 @@ const runUser = function(app){
     
   })
   app.post("/register",async (req, res, next) => {
-    const reg = await register(req.body)
+    const reg = await register(req)
     const resp = {
       data : reg.data,
       msg : reg.msg
