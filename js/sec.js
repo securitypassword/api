75a51a091426e98b525a61825b5c378663d71d06
martin-sainos-demian <martin.sainos.demian@gmail.com>
2022-11-20 00:49:45 -0600

    window.crypto.subtle

--- js/sec.js
+++ js/sec.js
@@ -112,7 +112,7 @@ const generateKeys = async function (){
     })
     const publicKey = keyPair.publicKey
     const privateKey = keyPair.privateKey
-    const cryptoSubtle = window.crypto.Subtle
+    const cryptoSubtle = window.crypto.subtle
     const publicKeyTxt = await cryptoSubtle.exportKey('pkcs8', publicKey)
     const privateKeyTxt = await cryptoSubtle.exportKey('pkcs8', privateKey)
     await setKey('public',publicKeyTxt)
