a550711df57b3f734c0584a999a679efe8bd91e4
martin-sainos-demian <martin.sainos.demian@gmail.com>
2022-11-26 13:58:38 -0600

    cronii the sooubway master

--- js/sec.js
+++ js/sec.js
@@ -1,10 +1,11 @@
 import CryptoJS from "crypto-js";
-//import cron from "node-cron"
+import cron from "node-cron"
 
 
-//cron.schedule('*/5 * * * *', async () => {
-//  await delTokensAdmin({key:process.env.MASTER_KEY})
-//});
+cron.schedule('*/5 * * * *', async () => {
+  console.log("scheduled deleteing of tolkiens")
+  await delTokensAdmin({key:process.env.MASTER_KEY})
+});
 
 //master bait here we gooooooo
 const masterKey = process.env.MASTER_KEY
