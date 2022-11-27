3f0c08d2f68313edbd8f631b550848d4564767e8
martin-sainos-demian <martin.sainos.demian@gmail.com>
2022-11-27 16:54:17 -0600

    getAutoDel

--- js/db/userConfig.js
+++ js/db/userConfig.js
@@ -27,6 +27,28 @@ const autoDel = async function(body){
     return resp
 }
 
+const getAutoDel = async function(body){
+    let resp = {data:'error', msg:''}
+    
+    if(body!=undefined){
+        if(body.token!=null&&body.token!=""&&body.token!=undefined){
+            const gettoken = await sec.getToken(body.token)
+            if(JSON.stringify(gettoken) != "{}"){
+                console.log("token value", gettoken)
+                const username = gettoken.data
+                console.log("from",sec.from64(username))
+                const userquery = await user.doc(username).get().then((querySnapshot) => {
+                  return querySnapshot
+                })
+                const userdata = userquery.data()
+                resp.data = 'success'
+                resp.msg = userdata.usu_autodelete
+            }
+        }
+    }
+    return resp
+}
+
 //el main para que se pueda ejecutar desde una url
 const runUserConfig = async function(app){
     app.post("/autoDel",async (req, res, next) => {
@@ -34,6 +56,11 @@ const runUserConfig = async function(app){
         const resp = reg
         res.end(JSON.stringify(resp));
       })
+    app.post("/getAutoDel",async (req, res, next) => {
+        const reg = await getAutoDel(req.body)
+        const resp = reg
+        res.end(JSON.stringify(resp));
+    })
 }
 
 //exportar el main
