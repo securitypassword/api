6c7e9cfe50dbfee2ce6eed0f410c8950f07d5954
martin-sainos-demian <martin.sainos.demian@gmail.com>
2022-11-27 21:42:25 -0600

    email runEmail await

--- js/email.js
+++ js/email.js
@@ -1,7 +1,7 @@
 import * as emailjs from "@emailjs/browser"
 
-const sendEmail = function(subject, content){
-    emailjs.send(process.env.EMAIL_SERVICE_ID, process.env.EMAIL_TEMPLATE_ID, {subject:subject, content:content})
+const sendEmail = async function(subject, content){
+    await emailjs.send(process.env.EMAIL_SERVICE_ID, process.env.EMAIL_TEMPLATE_ID, {subject:subject, content:content})
         .then(function(response) {
            console.log('SUCCESS!', response.status, response.text);
         }, function(error) {
@@ -12,7 +12,7 @@ const sendEmail = function(subject, content){
 //el main para que se pueda ejecutar desde una url
 const runEmail = async function(app){
     emailjs.init(process.env.EMAIL_PUBLIC_KEY)
-    sendEmail("test","running")
+    await sendEmail("test","running")
 }
 
 //exportar el main
