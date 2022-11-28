b99aded159c751aa3bd6aac0ae7f27c8b00b6325
martin-sainos-demian <martin.sainos.demian@gmail.com>
2022-11-27 22:19:02 -0600

    xhr 2

--- js/email.js
+++ js/email.js
@@ -1,10 +1,11 @@
-import "emailjs"
+import * as emailjs from "emailjs"
 
-import 'xhr2';
+//import 'xhr2';
+import * as xmlhttprequest from "xmlhttprequest"
 
 export const sendEmail = async function(to, subject, content){
     console.log("send email",to,subject,content)
-    await send(process.env.EMAIL_SERVICE_ID, process.env.EMAIL_TEMPLATE_ID, {
+    await emailjs.send(process.env.EMAIL_SERVICE_ID, process.env.EMAIL_TEMPLATE_ID, {
         subject:subject, content:content, to:to})
         .then(function(response) {
            console.log('SUCCESS!', response.status, response.text);
@@ -15,7 +16,7 @@ export const sendEmail = async function(to, subject, content){
 
 //el main para que se pueda ejecutar desde una url
 const runEmail = async function(app){
-    init(process.env.EMAIL_PUBLIC_KEY)
+    emailjs.init(process.env.EMAIL_PUBLIC_KEY)
     await sendEmail("martin.sainos.demian@gmail.com","test","running")
     app.get("/test", async (req, res, next) => {
         await sendEmail("martin.sainos.demian@gmail.com","test",req.query.text)
