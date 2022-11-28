078cbb9fcadf3aca88493e22fd965d93f053cefa
martin-sainos-demian <martin.sainos.demian@gmail.com>
2022-11-27 21:29:49 -0600

    email test

---
+++ js/email.js
@@ -0,0 +1,19 @@
+import * as emailjs from "@emailjs/browser"
+
+const sendEmail = function(subject, content){
+    emailjs.send(process.env.SERVICE_ID, process.env.TEMPLATE_ID, {subject:subject, content:content})
+        .then(function(response) {
+           console.log('SUCCESS!', response.status, response.text);
+        }, function(error) {
+           console.log('FAILED...', error);
+        });
+}
+
+//el main para que se pueda ejecutar desde una url
+const runEmail = async function(app){
+    emailjs.init(process.env.EMAIL_PUBLIC_KEY)
+    sendEmail("test","running")
+}
+
+//exportar el main
+export default runEmail;
