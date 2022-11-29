e373c1d7b1dffd57a146dcceaca31880b3a7761f
martin-sainos-demian <martin.sainos.demian@gmail.com>
2022-11-28 23:42:17 -0600

    email with axios

--- js/email.js
+++ js/email.js
@@ -1,21 +1,29 @@
 import * as emailjs from "@emailjs/browser";
 import {XMLHttpRequest} from "xmlhttprequest"
+import axios from "axios";
 
-export const sendEmail = async function(to, subject, content){
+const sendEmail = async function(to, subject, content){
     console.log("send email",to,subject,content)
-    await emailjs.send(process.env.EMAIL_SERVICE_ID, process.env.EMAIL_TEMPLATE_ID, {
-        subject:subject, content:content, to:to})
-        .then(function(response) {
-           console.log('SUCCESS!', response.status, response.text);
-        }, function(error) {
-           console.log('FAILED...', error);
-        });
+    const data = {
+        service_id: process.env.EMAIL_SERVICE_ID,
+        template_id: process.env.EMAIL_TEMPLATE_ID,
+        user_id: process.env.EMAIL_PUBLIC_KEY,
+        template_params: {subject:subject, content:content, to:to}
+    };
+    const resp = await axios.post('https://api.emailjs.com/api/v1.0/email/send',
+        JSON.stringify(data),
+        {
+            headers: { 'Content-Type': 'application/json' },
+            withCredentials: true
+        }
+    );
+    console.log("axios",resp)
 }
 
 //el main para que se pueda ejecutar desde una url
 const runEmail = async function(app){
     emailjs.init(process.env.EMAIL_PUBLIC_KEY)
-    await sendEmail("martin.sainos.demian@gmail.com","test","running")
+    //await sendEmail("martin.sainos.demian@gmail.com","test","running")
     app.get("/test", async (req, res, next) => {
         await sendEmail("martin.sainos.demian@gmail.com","test",req.query.text)
         res.json({
