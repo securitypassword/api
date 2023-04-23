945975c68fe3fc0913b2cb99b70e3f55df02071a
martin-sainos-demian <martin.sainos.demian@gmail.com>
2023-04-22 21:05:12 -0600

    cors

--- index.js
+++ index.js
@@ -6,25 +6,26 @@ import runReg from "./js/db/reg.js";
 import runUserConfig from "./js/db/userConfig.js"
 import runEmail from "./js/email.js";
 import runAsk from "./js/db/ask.js";
-import bodyParser from "body-parser";
 import cors from 'cors';
 var app = express();
  //get PORT from the server
  //obtener el PUERTO del server donde hosteamos
 const PORT = process.env.PORT;
+var allowedOrigins = ['http://localhost:3000', 'http://securitypassword.github.io'];
 
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
-})); 
-
-//global
-//que si no da error
-const corsConfig = {
-    credentials: true,
-    origin: true,
-};
-app.use(cors(corsConfig));
+app.use(cors({
+  origin: function(origin, callback){
+    // allow requests with no origin 
+    // (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 //end of global
 
 //ejecutar el main de user.js
