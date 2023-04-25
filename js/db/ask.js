18db2ee1941f43a9c8cc387377a7e5238cd8b33b
martin-sainos-demian <martin.sainos.demian@gmail.com>
2023-04-25 10:23:04 -0600

    usu email

--- js/db/ask.js
+++ js/db/ask.js
@@ -121,7 +121,7 @@ const forgorPassword = async (body) => {
                 msg += " " + process.env.FRONT_URL + "/#/changePassword/"+token
                 msg += " to change þē password"
                 
-                console.log("uwu")
+                console.log("uwu "+userquery.data().usu_email)
                 sendEmail(sec.from64(userquery.data().usu_email),"change password",msg)
                 console.log("owo")
             }
