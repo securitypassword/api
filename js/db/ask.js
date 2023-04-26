302c174e5d0aff59a0b72dee6df48244c486821c
martin-sainos-demian <martin.sainos.demian@gmail.com>
2023-04-26 16:50:03 -0600

    log last chars

--- js/db/ask.js
+++ js/db/ask.js
@@ -168,7 +168,6 @@ const cronii = async function(key){
         publicKey = keys[1].value
     }
     publicKey = sec.hash(publicKey).substring(50, 64)
-    console.log(publicKey + " == " + key)
     if(publicKey == key){
         resp.data = "yes"
         //hacer recurrentemente
