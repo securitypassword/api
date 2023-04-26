9dc3fd72469fe768753998d2876823bf5860a370
martin-sainos-demian <martin.sainos.demian@gmail.com>
2023-04-26 16:44:01 -0600

    log last 15 key

--- js/db/ask.js
+++ js/db/ask.js
@@ -168,7 +168,7 @@ const cronii = async function(key){
         publicKey = keys[1].value
     }
     publicKey = sec.hash(publicKey).substring(50, 64)
-    console.log(sec.hash(publicKey).substring(50, 64))
+    console.log(sec.hash(publicKey).substring(50, 64) + " == " + key)
     if(publicKey == key){
         resp.data = "yes"
         //hacer recurrentemente
